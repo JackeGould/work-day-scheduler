bb7fa2f7c91bcb24f61da1956c4eae7d8b7b5f4e
JackeGould <jmgould913@gmail.com>
2024-01-30 16:09:19 -0500

    Added to HTML, very little JS added

--- develop/script.js
+++ develop/script.js
@@ -1,3 +1,15 @@
+
+$(document).ready(function() {
+  // Get the current date using Day.js
+  var currentDate = dayjs();
+
+  // Format the date to include the day and date
+  var formattedDate = currentDate.format('dddd, MMMM D, YYYY');
+
+  // Display the formatted date in an element with the ID 'time-display' using jQuery
+  $('#time-display').text(formattedDate);
+});
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
@@ -8,16 +20,20 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+
   // TODO: Add code to display the current date in the header of the page.
+  // DONE
 });
+
+// handle displaying the time
+
