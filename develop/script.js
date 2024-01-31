d605f27080d587b9675c3b4fc965e4b0804549f7
JackeGould <jmgould913@gmail.com>
2024-01-30 20:54:40 -0500

    Code added to JS, bug fixes in HTML

--- develop/script.js
+++ develop/script.js
@@ -10,10 +10,94 @@ $(document).ready(function() {
   $('#time-display').text(formattedDate);
 });
 
+$(document).ready(function () {
+  // saveBtn click listener 
+  $(".saveBtn").on("click", function () {
+      // Get nearby values of the description in JQuery
+      var text = $(this).siblings(".description").val();
+      var time = $(this).parent().attr("id");
+      localStorage.setItem(time, text);
+      
+
+  })
+
+  function timeTracker() {
+    // get current number of hours.
+    var timeNow = dayjs().hour();
+  
+    // loop over time blocks
+    $(".time-block").each(function () {
+        var blockTime = parseInt($(this).attr("id").split("hour-")[1]);
+
+        console.log(blockTime);
+  
+        // To check the time and add the classes for background indicators
+        if (blockTime < timeNow) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+        else if (blockTime === timeNow) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+  
+        }
+    })
+  }
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-1 .description").val(localStorage.getItem("hour-1"));
+  $("#hour-2 .description").val(localStorage.getItem("hour-2"));
+  $("#hour-3 .description").val(localStorage.getItem("hour-3"));
+  $("#hour-4 .description").val(localStorage.getItem("hour-4"));
+  $("#hour-5 .description").val(localStorage.getItem("hour-5"));
+
+
+timeTracker();
+ // Save text in local storage
+      
+  })
+
+// function timeTracker() {
+//   //get current number of hours.
+//   var timeNow = moment().hour();
+
+//   // loop over time blocks
+//   $(".time-block").each(function () {
+//       var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+//       // To check the time and add the classes for background indicators
+//       if (blockTime < timeNow) {
+//           $(this).removeClass("future");
+//           $(this).removeClass("present");
+//           $(this).addClass("past");
+//       }
+//       else if (blockTime === timeNow) {
+//           $(this).removeClass("past");
+//           $(this).removeClass("future");
+//           $(this).addClass("present");
+//       }
+//       else {
+//           $(this).removeClass("present");
+//           $(this).removeClass("past");
+//           $(this).addClass("future");
+
+//       }
+//   })
+// }
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -33,7 +117,19 @@ $(function () {
 
   // TODO: Add code to display the current date in the header of the page.
   // DONE
-});
 
 // handle displaying the time
 
+    // Get item from local storage if any
+//     $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+//     $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+//     $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+//     $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+//     $("#hour-1 .description").val(localStorage.getItem("hour-1"));
+//     $("#hour-2 .description").val(localStorage.getItem("hour-2"));
+//     $("#hour-3 .description").val(localStorage.getItem("hour-3"));
+//     $("#hour-4 .description").val(localStorage.getItem("hour-4"));
+//     $("#hour-5 .description").val(localStorage.getItem("hour-5"));
+
+
+// timeTracker();
